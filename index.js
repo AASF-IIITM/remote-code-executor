f2e50d38618e599429f49f1d74da3c83c493d99d
Poorvi Vaish <img_2019047@iiitm.ac.in>
2020-12-17 00:10:57 +0530

    modified index.js

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log("Hello World")
+console.log("Hello World ")
+const express = require('express')
+const app = express()
+
+app.get('/', (req, res) => {
+  res.send('Hi!')
+})
+app.get('/', (req, res) => {
+    res.send('hey Poorvi!')
+  })
+app.listen(3000, () => console.log('Server ready'))
