5ef0872d0354040a69e191a5650f8b9e1295d46c
Manish Kumar <manishprivet@protonmail.com>
2020-12-20 23:40:48 +0530

    Added Promises

--- index.js
+++ index.js
@@ -1,21 +1,39 @@
 
-const { execute } = require("./utils/file-executor.js");
-const {exec} = require("child_process");
-const fs = require("fs");
+const { execPromise, writeFilePromise, deleteFilePromise } = require("./utils/file-executor.js");
 
 const data = `console.log("Hello World!!!");`;
 
-fs.writeFile("temp/code.js", data, (err) => {
-  if (err) console.log(err);
-  console.log("Successfully Written to File.");
-  exec("node temp/code.js", function(error, stdout, stderr) {
-    console.log(error);
-    console.log(stdout);
-    console.log(stderr);
+// fs.writeFile("temp/code.js", data, (err) => {
+//   if (err) console.log(err);
+//   console.log("Successfully Written to File.");
+//   exec("node temp/code.js", function(error, stdout, stderr) {
+//     console.log(error);
+//     console.log(stdout);
+//     console.log(stderr);
     
-    fs.unlink("temp/code.js", () => {
-      console.log("File Deleted!!!")
-    });
-  });
-});
-// execute();
+//     fs.unlink("temp/code.js", () => {
+//       console.log("File Deleted!!!")
+//     });
+//   });
+// });
+
+const test =  () => {
+  try {
+    await writeFilePromise("temp/code.js", data);
+    const output = await execPromise("node temp/code.js");
+    console.log(output);
+    await deleteFilePromise("temp/code.js");
+  } catch(err) {
+    console.log(err);
+  }
+  
+  // writeFilePromise("temp/code.js", data)
+  //   .then(() => execPromise("node temp/code.js"))
+  //   .then((output) => {
+  //     console.log(output);
+  //     return deleteFilePromise("temp/code.js");
+  //   })
+  //   .catch((err) => console.log(err))
+}
+
+test();
--- utils/file-executor.js
+++ utils/file-executor.js
@@ -1,11 +1,37 @@
 const { exec } = require('child_process');
+const fs = require("fs");
 
-const execute = () => {
-    exec("node temp/code.js", function(error, stdout, stderr) {
-        console.log(error);
-        console.log(stdout);
-        console.log(stderr)
+// const execute = () => {
+//     exec("node temp/code.js", function(error, stdout, stderr) {
+//         console.log(error);
+//         console.log(stdout);
+//         console.log(stderr)
+//     });
+// }
+
+const execPromise = (command) => new Promise((resolve, reject) => {
+    exec(command, function(error, stdout, stderr) {
+        if(error) reject(error);
+        else {
+            const isError = Boolean(stderr);
+            resolve({ isError, output: stdout || stderr });
+        }
     });
-}
+});
 
-module.exports = { execute };
+const writeFilePromise = (location, data) => new Promise((resolve, reject) => {
+    fs.writeFile(location, data, function(err) {
+        if(err) reject(err);
+        else resolve();
+    })
+});
+
+const deleteFilePromise = (location) => new Promise((resolve, reject) => {
+    fs.unlink(location, (err) => {
+        if(err) reject(err);
+        else resolve();
+    })
+})
+
+
+module.exports = {  execPromise, writeFilePromise, deleteFilePromise };
